380b2b18d79b0dc6b23f761e9b55077833919a68
Pedro Gabriel <pplanel@gmail.com>
2024-07-09 14:50:23 -0300

    feat: Use the Repr struct directly when calling serve

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,13 @@ fn main() -> anyhow::Result<()> {
     pretty_env_logger::init();
 
     let args = App::parse();
+
+    let manifest = Manifest::parse(&args.global_opts.proj)?;
+    let ir = traverser::traverse(&args.global_opts, &manifest)?;
+    let cytoscape_repr = cytoscape::from_ir(ir);
+
     match args.command {
         cli::Command::Dump(opts) => {
-            let manifest = Manifest::parse(&args.global_opts.proj)?;
-            let ir = traverser::traverse(&args.global_opts, &manifest)?;
-            let cytoscape_repr = cytoscape::from_ir(ir);
             std::fs::write(
                 &opts.output,
                 serde_json::to_string_pretty(&cytoscape_repr).expect("Failed to pretty-print JSON"),
@@ -29,7 +31,7 @@ fn main() -> anyhow::Result<()> {
         }
         cli::Command::Serve(opts) => {
             log::info!("Server starting at port :{}", opts.server_port);
-            web_server::serve(opts.server_port);
+            web_server::serve(cytoscape_repr, opts.server_port);
             log::info!("Shutting down server");
         }
     }
--- src/web_server.rs
+++ src/web_server.rs
@@ -2,17 +2,18 @@ use std::fs;
 
 use rouille::router;
 
+use crate::output::cytoscape::Repr;
+
 const INDEX: &str = include_str!("../index.html");
 
-pub fn serve(server_port: u32) {
+pub fn serve(repr: Repr, server_port: u32) {
     rouille::start_server(format!("127.0.0.1:{}", server_port), move |request| {
-        let content = fs::read_to_string("codebase-dump.json").expect("file to exist");
         router!(request,
             (GET) ["/"] => {
                 rouille::Response::html(INDEX)
             },
             (GET) ["/codebase-dump.json"] => {
-                rouille::Response::from_data("application/json", content)
+                rouille::Response::json(&repr)
             },
             _ => rouille::Response::empty_404()
         )
